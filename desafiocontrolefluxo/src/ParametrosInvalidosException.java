623b17b93aef5e6ff403c4484ae7e810174f003b
Brandon48 <brandonrsc48@gmail.com>
2023-12-07 00:08:45 -0300

    Primeiro desafio java básico

---
+++ desafiocontrolefluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+
+public class ParametrosInvalidosException extends Exception{
+
+    public ParametrosInvalidosException(String string) {
+    }
+ 
+
+    public ParametrosInvalidosException() {
+        
+    }
+}
+
+//nome da exceção que deve ser lançada: ParametrosInvalidosException
+//MENSAGEM: "O segundo parâmetro deve ser maior que o primeiro.".
